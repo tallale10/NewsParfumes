32b15932d04f68e62bacc1fb2aedb0b3b101da58
Your Name <you@example.com>
2023-12-12 00:07:44 +0100

    first commit

---
+++ src/Learnmore.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+
+
+
+
+function Learnmore() {
+    return (
+      <div>
+        <Navbar />
+        <Driver />
+        <Footer />
+      </div>
+    );
+  }
+  
+  export default Learnmore;
